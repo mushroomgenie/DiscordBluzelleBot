ec6b5a4a789e343bb894d8e212e6d3e65f268ad7
SatyaSnehith <nsssnehit76@gmail.com>
2021-06-30 01:59:59 +0530

    added buttons to ".validators" command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Discord from "discord.js"
 import config from "./config.json"
 import {bluzelle} from "@bluzelle/sdk-js"
 import axios from "axios"
+import commaNumber from 'comma-number'
 import DiscordButton, { MessageButton } from "discord-buttons"
 
 const uuid = "0f4016c4-47f6-4cdb-ad48-4a6c3971bc24" 
@@ -13,8 +14,10 @@ const sdk = await bluzelle({
     gasPrice:0.002
 })
 
+
 const client = new Discord.Client()
-const dButton = DiscordButton(client);
+DiscordButton(client)
+
 const prefix = "."
 
 // .ping
@@ -53,11 +56,6 @@ client.on("message",(message) => {
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
 
-    // const rButton = new MessageButton()
-    // .setLabel('🔄')
-    // .setStyle('gray')
-    // .setID('refreshPrice')
-
     if(message.content === ".price") {
         axios.get("https://api.coingecko.com/api/v3/simple/price?ids=bluzelle&vs_currencies=usd&include_24hr_change=true")
         .then((response) => {
@@ -65,52 +63,97 @@ client.on("message",(message) => {
             let color = change >= 0 ? '#8bc34a' : '#f44336';
             let embed = new Discord.MessageEmbed()
             .setColor('#FFFFFF')
-            .setTitle(`The price in USD is $${response.data.bluzelle.usd}`)
-            .setDescription('Price Change(24h): ' + (change >= 0 ? '+' : '') + change + "%")
+            .setTitle('Price')
+            .addField('USD', '$' + response.data.bluzelle.usd)
+            .addField('Price Change(24h)', (change >= 0 ? '+' : '') + change + "%")
             message.reply(embed);
-            // message.reply('', {
-            //     buttons: [rButton],
-            //     embed: embed
-            // })
+
         })
     }
 })
 
-// client.on('clickButton', (button) => {
 
-//     if (button.id == 'refreshPrice') {
-//         button.defer();
-//         console.log(button);
-//     }
-// })
 
+
+//.validators
 client.on("message",(message) => {
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
 
     if(message.content.startsWith(".validators")){
         const net = message.content.split(" ")[1]
-        switch(net){
+         
+        const mvButton = new MessageButton()
+        .setStyle('green')
+        .setLabel('mainnet')
+        .setID('mainnet_validators')
+        
+        const tvButton = new MessageButton()
+        .setLabel('testnet')
+        .setStyle('blurple')
+        .setID('testnet_validators')
+
+        switch(net) {
+            case "t":
+            case "test":
             case "testnet":
-                axios.get("https://client.sentry.testnet.private.bluzelle.com:26657/validators").then((response) => {
-                    const reply = `Block Height ${response.data.result.block_height} Count:${response.data.result.count}`
-                    message.reply(reply)
-                })
+                sendTestnetValidators(message.channel)
                 break
+            case "m":
+            case "main":
             case "mainnet":
-                axios.get("http://sandbox.sentry.net.bluzelle.com:26657/validators").then((response) => {
-                    const reply = `Block Height ${response.data.result.block_height} Count:${response.data.result.count}`
-                    message.reply(reply)
-                })
+                sendMainnetValidators(message.channel)
                 break
             default:
-                axios.get("https://client.sentry.testnet.private.bluzelle.com:26657/validators").then((response) => {
-                    const reply = `Block Height ${response.data.result.block_height} Count:${response.data.result.count}`
-                    message.reply(reply)
+                message.reply('**Validators from?**', {
+                    buttons: [mvButton, tvButton],
                 })
         }
     }
 })
+
+client.on('clickButton', (button) => {
+    if (button.id == 'mainnet_validators') {
+        button.defer();
+        sendMainnetValidators(button.channel)
+    } else if (button.id == 'testnet_validators') {
+        button.defer();
+        sendTestnetValidators(button.channel)
+    }
+})
+
+function sendMainnetValidators(message) {
+    axios.get("http://sandbox.sentry.net.bluzelle.com:26657/validators").then((response) => {
+        let embed = new Discord.MessageEmbed()
+        .setColor('#3BA55D')
+        .setTitle("Active Validators of mainnet")
+        .addField("Count", commaNumber(response.data.result.count))
+        .addField("Block Height", commaNumber(response.data.result.block_height))
+        .addField("Voting Power", commaNumber(getTotalVotingPower(response.data.result.validators)))
+        message.send(embed);
+    })
+}
+
+function sendTestnetValidators(message) {
+    axios.get("https://client.sentry.testnet.private.bluzelle.com:26657/validators").then((response) => {
+        let embed = new Discord.MessageEmbed()
+        .setColor('#5865F2')
+        .setTitle("Active Validators of testnet")
+        .addField("Count", commaNumber(response.data.result.count))
+        .addField("Block Height", commaNumber(response.data.result.block_height))
+        .addField("Voting Power", commaNumber(getTotalVotingPower(response.data.result.validators)))
+        message.send(embed);                
+    })
+}
+
+function getTotalVotingPower(validators) {
+    let vp = 0;
+    for (let i = 0; i < validators.length; ++i) {
+        vp += parseInt(validators[i].voting_power)
+    }
+    return vp
+}
+
 
 client.on("message",(message) => {
     if(message.author.bot) return;
