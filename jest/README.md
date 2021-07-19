a38ad1e4738893cced7fcd307e77f087e76e4b2e
SatyaSnehith <nsssnehit76@gmail.com>
2021-07-19 22:35:30 +0530

    added jest

---
+++ jest/README.md
@@ -0,0 +1,14 @@
+# Test with Jest
+Jest is a JavaScript Testing Framework with a focus on simplicity.
+## Usage
+#### Step 1. Open terminal in the jest folder and install all the required dependencies.
+  ```
+  npm install
+  ```
+#### Step 2. Run tests.
+  ```
+  npm test
+  ```
+## ScreenShot
+
+![41](../images/41.png)
--- jest/index.spec.ts
+++ jest/index.spec.ts
@@ -20,29 +20,29 @@ describe("Message Handler", () => {
 
     it("should call price command", async () => {
         message.content = '.price';
-        await messageHandler(message);
         let response = await axios.get("https://api.coingecko.com/api/v3/coins/bluzelle?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false")
+        await messageHandler(message);
         expect(message.channel.send).toHaveBeenCalledWith(response.data.market_data.current_price.usd)
     })
 
     it("should call market command", async () => {
         message.content = '.market';
-        await messageHandler(message);
         let response = await axios.get("https://api.coingecko.com/api/v3/coins/bluzelle?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false")
+        await messageHandler(message);
         expect(message.channel.send).toHaveBeenCalledWith(response.data.market_data.market_cap.usd)
     })
 
     it("should call validators command", async () => {
         message.content = '.validators';
-        await messageHandler(message);
         let response = await axios.get("https://client.sentry.testnet.private.bluzelle.com:26657/validators")
+        await messageHandler(message);
         expect(message.channel.send).toHaveBeenCalledWith(response.data.result.count)
     })
 
     it("should call blocks command", async () => {
         message.content = '.blocks';
-        await messageHandler(message);
         let response = await axios.get("https://client.sentry.testnet.private.bluzelle.com:26657/status")
+        await messageHandler(message);
         expect(message.channel.send).toHaveBeenCalledWith(response.data.result.sync_info.latest_block_height)
     })
 
